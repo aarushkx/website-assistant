337d5a01c4b48ca38529f4ed248ee0e39644d341
Aarush Kumar <2005aarushkumar@gmail.com>
2025-07-28 23:29:35 +0530

    feat: setup upstash rag-chat and redis config

---
+++ lib/rag-chat.ts
@@ -0,0 +1,16 @@
+import { OpenAIChatLanguageModel, RAGChat } from "@upstash/rag-chat";
+import { createOpenAI } from "@ai-sdk/openai";
+import { redis } from "./redis";
+
+const model = createOpenAI({
+    baseURL: "http://localhost:11434/v1",
+    apiKey: "", // Local Ollama doesn't require an API key
+});
+
+const llamaModel = model("llama2") as OpenAIChatLanguageModel;
+
+export const ragChat = new RAGChat({
+    model: llamaModel,
+    redis: redis,
+    debug: true, // TODO: Remove this.
+});
